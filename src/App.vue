cc8ee81f019a7cf24f2be0466a66c48f486351f4
Ahmed <a7med.reda224@gmail.com>
2020-04-20 19:35:56 +0200

    create admin calls in adminService file

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ export default {
   created() {
     this.$http.interceptors.response.use(undefined, (err) => new Promise(() => {
       // eslint-disable-next-line no-underscore-dangle
-      if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
-        this.$store.dispatch('logout');
+      if (err.status === 401) {
+        this.logout();
       }
       throw err;
     }));
---
+++ src/services/adminService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const adminCall = axios.create({
+  baseURL: 'http://localhost:5000',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+});
+
+export default {
+  getDepartments() {
+    return adminCall.get('/api/Admin/GetDepartments');
+  },
+  postDepartment() {
+    return adminCall.post('/api/Admin/RegisterAgent');
+  },
+};

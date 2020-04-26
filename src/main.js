515c4e68e96e6ef6d38e36182c83711a46e08090
Ahmed <a7med.reda224@gmail.com>
2020-04-26 17:27:54 +0200

    globalize the base url & axios auth

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Axios from 'axios';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -9,6 +10,11 @@ import store from './store';
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.config.productionTip = false;
+Axios.defaults.baseURL = 'http://localhost:5000';
+const token = localStorage.getItem('token');
+if (token) {
+  Axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+}
 
 new Vue({
   router,

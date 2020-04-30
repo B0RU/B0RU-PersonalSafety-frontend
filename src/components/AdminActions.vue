5fad3fd37f29e0bbb0adcce5585c2e8e4e9f6721
Ahmed <a7med.reda224@gmail.com>
2020-04-30 02:06:08 +0200

    add admin actions Component & jumbotron component

---
+++ src/components/AdminActions.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mainActions">
+        <div class="col-md-6">
+            <jumbotron-component name="Client" action="resetClient"></jumbotron-component>
+        </div>
+        <div class="col-md-6">
+            <jumbotron-component name="Rescuer" action="resetRescuer"></jumbotron-component>
+        </div>
+    </div>
+</template>
+
+<script>
+import jumbotronComponent from './JumbotronComponent.vue';
+
+export default {
+  components: {
+    jumbotronComponent,
+
+  },
+};
+</script>
+
+<style scoped>
+.mainActions {
+    display: flex;
+    width: 80%;
+    float: right;
+}
+</style>
---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+  <b-jumbotron class="jumbo">
+      <h3>Reset {{name}} Connection</h3>
+      <p>Reset the trackers for a particular {{name}}.</p>
+    <b-form @submit="onSubmit">
+    <b-form-input
+          id="input-1"
+          v-model="email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+    <b-button class="submit-btn" variant="danger" type="submit">Reset</b-button>
+    </b-form>
+  </b-jumbotron>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'jumbotron',
+  props: {
+    name: {
+      type: String,
+    },
+    action: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      email: '',
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$store.dispatch(`register/${this.action}`, this.email);
+      console.log(`register/this is ${this.action}`);
+    },
+  },
+  computed: {
+    getMessages() {
+      return this.$store.getters.messages;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.jumbo{
+    margin: 20px;
+    color: #000;
+}
+#input-1{
+    display: inline-block;
+    width: 70%;
+}
+.submit-btn {
+    float: right;
+}
+</style>

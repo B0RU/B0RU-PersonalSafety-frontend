044cf3b0e03fc361e94f1ae53fa4cc6a4143b537
Ahmed <a7med.reda224@gmail.com>
2020-05-03 06:22:26 +0200

    manage side bar actions

--- src/components/actions.vue
+++ src/components/actions.vue
@@ -1,19 +1,38 @@
 <template>
   <div class='actions'>
-
+    <ul>
+      <li v-for="(action, index) in actions" :key="index">
+            {{action}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    actions: {
+      type: Array,
+    },
+  },
 };
 </script>
 
 <style scoped>
 .actions{
-    height: 200px;
     border-radius: 19px;
-    background: #5E657A
+    background: #5E657A;
+    padding: 20px 20px 20px 30px;
+    color: gray;
+}
+ul{
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: left;
+}
+li{
+  cursor: pointer;
+  margin: 5px;
 }
 </style>

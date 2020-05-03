998f0445181ecbff334546602b4e16eba5ff530a
Ahmed <a7med.reda224@gmail.com>
2020-05-03 06:21:07 +0200

    add requests component to manage and view all requests

---
+++ src/components/requests.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mainActions">
+    <h3>Monitor Requests</h3>
+    <p class="subhead">You can Monitor Department Requests</p>
+    <b-overlay :show="status" opacity="1" variant="transparent" spinner-variant="primary">
+    <requests-component :aria-hidden="show ? 'true' : null"></requests-component>
+    </b-overlay>
+  </div>
+</template>
+
+<script>
+import requestsComponent from './request.vue';
+
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  created() {
+    console.log(this.$route.name);
+  },
+  components: {
+    requestsComponent,
+  },
+  computed: {
+    getDepartments() {
+      return this.$store.state.manager.departments;
+    },
+    status() {
+      return this.$store.state.manager.status === 'loading';
+    },
+  },
+};
+</script>
+
+<style lang='scss'>
+
+.mainActions {
+  display: block;
+  text-align: left;
+  padding: 20px;
+}
+.subhead {
+  color: gray;
+}
+</style>

f0f432d082ff5c0ee5b4f6a32a2765377674c4b7
Ahmed <a7med.reda224@gmail.com>
2020-05-06 02:12:58 +0200

    add side bar to AdminPanel

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,28 +1,39 @@
 <template>
-    <div class="adminPanel">
-        <div class="side">
-            <router-link to="/admin/dashboard">
-                <add-button action="Dashboard"></add-button>
-            </router-link>
-            <router-link to="/admin/register-agent">
-                <add-button action="New Agent"></add-button>
-            </router-link>
-            <router-link to="/admin/register-manager">
-                <add-button action="New Manager"></add-button>
-            </router-link>
-        </div>
-        <div class="main-actions">
-        <router-view/>
-        </div>
+  <div class="adminPanel">
+    <div class="side">
+      <router-link to="/admin/dashboard">
+        <add-button action="Dashboard"></add-button>
+      </router-link>
+      <side-actions :actions="adminActions"></side-actions>
+      <side-actions :actions="coreActions"></side-actions>
     </div>
+    <div class="main-actions">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
 import addButton from '../components/newButton.vue';
+import sideActions from '../components/actions.vue';
 
 export default {
+  data() {
+    return {
+      adminActions: {
+        Requests: { url: 'requests', value: 'Requests' },
+        Events: { url: 'events', value: 'Events' },
+      },
+      coreActions: {
+        'Register Agent': { url: '/admin/register-agent', value: 'Register Agent' },
+        Managers: { url: '/admin/register-manager', value: 'Managers' },
+        'Regional Distribution': { url: '/admin/regional-distribution', value: 'Regional Distribution' },
+      },
+    };
+  },
   components: {
     addButton,
+    sideActions,
   },
   created() {
     this.getDepartments();
@@ -36,21 +47,21 @@ export default {
 </script>
 
 <style>
-.adminPanel{
+.adminPanel {
   display: flex;
-    margin-top: 30px;
+  margin-top: 30px;
 }
-.side{
-   width: 15%;
-    height: 100%;
-    margin-left: 25px;
+.side {
+  width: 15%;
+  height: 100%;
+  margin-left: 25px;
 }
 .main-actions {
-    width: 75%;
-    margin: 0 auto;
-    float: right;
-    background-color: #5E657A;
-    height: 500px;
-    border-radius: 15px;
+  width: 75%;
+  margin: 0 auto;
+  float: right;
+  background-color: #5e657a;
+  height: 500px;
+  border-radius: 15px;
 }
 </style>

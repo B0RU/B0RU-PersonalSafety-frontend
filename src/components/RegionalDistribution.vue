2e2845dc8047dd70ae670da0ce215dc6294542b8
Ahmed <a7med.reda224@gmail.com>
2020-05-09 18:41:18 +0200

    add regionalDistribution component for the adminPanel

---
+++ src/components/RegionalDistribution.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mainActions">
+    <h3>Regional Distribution</h3>
+    <p class="subhead">View and edit information about the geographical distribution of the system</p>
+    <distribution-table></distribution-table>
+  </div>
+</template>
+
+<script>
+import distributionTable from './DistributionTable.vue';
+
+export default {
+  components: {
+    distributionTable,
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.mainActions {
+  display: block;
+  text-align: left;
+  padding: 20px;
+}
+.subhead {
+  color: gray;
+}
+</style>

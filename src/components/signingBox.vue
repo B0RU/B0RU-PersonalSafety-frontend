1d9f0226198853a5dd4331ff98f00b05a74cc55d
Ahmed <a7med.reda224@gmail.com>
2020-05-20 07:35:05 +0200

    add signingBox component to display information about logged in user

---
+++ src/components/signingBox.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div v-if="isLoggedIn">
+      <div class="logIn-Out dropdown">
+       <span style="font-size: 1.1rem">
+           {{userName}} <b-icon-chevron-down></b-icon-chevron-down>
+       </span>
+        <br />
+       <span style="font-size: 1rem"> {{userRole}} </span>
+       <div class="dropdown-content">
+           <span style="color: gray; font-size: 0.9rem">logged In As:</span><br>
+           <span style="font-size: 1.1rem">{{userEmail}}</span><br>
+           <b-button variant="danger"  @click="logout">log Out</b-button>
+       </div>
+      </div>
+    </div>
+    <router-link :to="{name: 'login'}" v-else>
+      <div class="logIn-Out" style="padding-top: 15px">LOGIN</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    },
+    userName() {
+      return this.$store.state.accountDetails.fullName;
+    },
+    userRole() {
+      return this.$store.state.accountDetails.roles.toString();
+    },
+    userEmail() {
+      return this.$store.state.accountDetails.email;
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+        .then(() => this.$router.push('/login'))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.logIn-Out{
+    float: right;
+    font-family: "Roboto", sans-serif;
+    font-size: 20px;
+    line-height: 1;
+    padding-right: 4%;
+    text-align: left;
+    text-decoration: none;
+    color: white;
+    cursor: pointer;
+}
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #5e657a;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  border-radius: 15px;
+  padding: 20px;
+  z-index: 1;
+  right: 0;
+  text-align: center;
+  .btn {
+      margin-top: 20px;
+  }
+}
+
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>

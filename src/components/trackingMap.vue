6ffb30456798cce508b50d6b79f13099df1c96af
Ahmed <a7med.reda224@gmail.com>
2020-05-29 23:05:06 +0200

    fullt functioned trackingMap

--- src/components/trackingMap.vue
+++ src/components/trackingMap.vue
@@ -1,24 +1,43 @@
 <template>
-  <GmapMap ref="trackingMap" :center="center" :zoom="zoom" style="height:100%; ">
-    <GmapCluster :grid-size="30">
-        <GmapMarker v-for="department in departments" :key="department.departmentId"
+  <GmapMap ref="trackingMap" :center="center" :zoom="zoom" style="height:100%;" @click="restoreMap">
+    <div v-if="visible">
+        <GmapMarker v-for="department in departments" :key="department.id"
             ref="departmentMarker"
             :position="{lat: department.latitude, lng:department.longitude}"
-            :icon='icons[department.authorityTypeName].icon'
+            :icon='departmentIcons[department.authorityTypeName].icon'
+            :visible='visible'
+            :clickable='true'
+            @click="getRescuers(department.id, department.departmentName)"
         >
+          <gmap-info-window :opened='ifw'>
+            {{department.authorityTypeName}} <br> {{department.distributionName}}
+          </gmap-info-window>
         </GmapMarker>
-    </GmapCluster>
+    </div>
+    <div v-else>
+      <GmapMarker v-for="rescuer in rescuers" :key="rescuer.rescuerEmail"
+      ref="rescuerMarker"
+      :position="rescuer.position"
+      :icon="rescuerIcons[rescuer.authorityTypeName].icon"
+      >
+
+      </GmapMarker>
+    </div>
   </GmapMap>
 </template>
 
 <script>
-import GmapCluster from 'vue2-google-maps/dist/components/cluster';
+// import GmapCluster from 'vue2-google-maps/dist/components/cluster';
 
 export default {
-  components: { GmapCluster },
+  // components: { GmapCluster },
   data() {
     return {
       map: null,
+      position: {
+        lat: 0,
+        lng: 0,
+      },
       myCoordinates: {
         lat: 0,
         lng: 0,
@@ -28,7 +47,12 @@ export default {
         lng: 31.638810825000014,
       },
       zoom: 6,
-      icons: {
+      ifw: false,
+      visible: true,
+      rescuers: [],
+      departmentName: '',
+      interval: null,
+      departmentIcons: {
         TowTruck: {
           icon: '/TowTruck.png',
         },
@@ -42,14 +66,34 @@ export default {
           icon: '/Firefighting.png',
         },
       },
+      rescuerIcons: {
+        TowTruck: {
+          icon: '/TowTruckRescuer.png',
+        },
+        Ambulance: {
+          icon: '/AmbulanceRescuer.png',
+        },
+        Police: {
+          icon: '/PoliceRescuer.png',
+        },
+        Firefighting: {
+          icon: '/FirefightingRescuer.png',
+        },
+      },
     };
   },
   created() {
+    this.$locationHub.$on('location-changed', this.locationChanged);
   },
   mounted() {
     this.$refs.trackingMap.$mapPromise.then((map) => {
       this.map = map;
     });
+    this.$locationHub.$on('location-changed', this.locationChanged);
+  },
+  beforeDestroy() {
+    this.$locationHub.$off('location-changed', this.locationChanged);
+    this.$locationHub.stop();
   },
   computed: {
     departments() {
@@ -57,9 +101,46 @@ export default {
     },
   },
   methods: {
+    getRescuers(id, name) {
+      console.log(this.$refs);
+      const { departments } = this.$store.state.manager;
+      const department = departments.filter((item) => item.id === id);
+      department[0].rescuersEmails.forEach((rescuer) => {
+        this.rescuers.push({ rescuerEmail: rescuer, authorityTypeName: department[0].authorityTypeName, position: { lat: null, lng: null } });
+      });
+      this.visible = false;
+      this.$locationHub.start().then(() => {
+        this.$locationHub.locationOpened(name);
+      });
+      this.rescuers.forEach((rescuer) => {
+        if (rescuer.position.lat === null && rescuer.position.lng === null) {
+          // eslint-disable-next-line no-param-reassign
+          rescuer.position = null;
+        }
+      });
+    },
+    locationChanged(payload) {
+      this.rescuers.forEach((rescuer) => {
+        if (payload.position.lat === null && payload.position.lng === null) {
+          // eslint-disable-next-line no-param-reassign
+          rescuer.position = null;
+        } else {
+          // eslint-disable-next-line no-param-reassign
+          rescuer.position = payload.position;
+        }
+      });
+    },
+    restoreMap() {
+      this.visible = true;
+      this.rescuers = [];
+      this.$locationHub.stop();
+    },
   },
 };
 </script>
 
 <style>
+.gm-style-iw{
+  color: black;
+}
 </style>
